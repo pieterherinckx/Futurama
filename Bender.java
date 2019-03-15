6b64de620343bc263ce7456dc568185718c9b35c
pieterherinckx <pieterherinckx@atheneumtienen.be>
2019-03-15 18:31:42 +0100

    Create Bender.java

---
+++ Bender.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bender here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bender extends Mover
+{
+    /**
+     * Act - do whatever the Bender wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(isKeyDown("right")){
+        moveRight();
+    }
+    }    
+}
